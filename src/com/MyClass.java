e403e8016224256a7b044b38ddd70648f73c0dc4
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-01-03 18:54:11 +0000

    Created MyClass.java
    Created class to test the generation from Modelio.

---
+++ src/com/MyClass.java
@@ -0,0 +1,11 @@
+package com;
+
+
+public class MyClass {
+    public void testOperation() {
+//begin of modifiable zone(JavaCode)......C/8e94e7e8-b196-445b-b3e6-c89db59a398a
+
+//end of modifiable zone(JavaCode)........E/8e94e7e8-b196-445b-b3e6-c89db59a398a
+    }
+
+}
