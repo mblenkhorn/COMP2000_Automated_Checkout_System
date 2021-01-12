fd78ec4576cda6e9dc3b60b3591a65dba6ad1e66
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-01-12 19:16:03 +0000

    Updated IModelSubject.java
    Updated addStock method in the interface so a class that implements it can use it.

--- src/com/IModelSubject.java
+++ src/com/IModelSubject.java
@@ -1,10 +1,12 @@
 package com;
 
 
+import javax.swing.*;
+
 public interface IModelSubject {
     void removeStock();
 
-    void addStock(Stock newStock);
+    void addStock(Stock newStock, JTextField barCodeInput, JTextField nameInput, JTextField priceInput);
 
     void editStock();
 
