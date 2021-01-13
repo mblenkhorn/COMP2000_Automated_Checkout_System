da62fcc7d2256124e3ce3a2ad3e0e3b74eb1b904
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-01-13 16:14:01 +0000

    Added Classes and Interfaces
    Added Customers.java, IControllerStrategy.java and IObserver.java to the project

---
+++ src/com/Customers.java
@@ -0,0 +1,27 @@
+package com;
+
+
+public class Customers implements IControllerStrategy, IObserver {
+    public Stock stock;
+
+    public Stock[] scannedItems;
+
+    public void PayWithCashorCard() {
+//begin of modifiable zone(JavaCode)......C/7a874ad9-e1c3-4f84-b03b-c3d7e0eb74bf
+
+//end of modifiable zone(JavaCode)........E/7a874ad9-e1c3-4f84-b03b-c3d7e0eb74bf
+    }
+
+    public void scanStock() {
+//begin of modifiable zone(JavaCode)......C/2876f45b-c7e4-4691-ac77-b313b037575e
+
+//end of modifiable zone(JavaCode)........E/2876f45b-c7e4-4691-ac77-b313b037575e
+    }
+
+    public void updateDatabase() {
+//begin of modifiable zone(JavaCode)......C/5bf0c6f0-d635-4c8f-b012-3b8498fe4812
+
+//end of modifiable zone(JavaCode)........E/5bf0c6f0-d635-4c8f-b012-3b8498fe4812
+    }
+
+}
