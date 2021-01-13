04b9e71f943dea0d50a829bfd73fa794b9210ed6
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-01-13 18:53:39 +0000

    Updated StockOrders
    Added a quantity attribute to make the setter work.

--- src/com/StockOrders.java
+++ src/com/StockOrders.java
@@ -2,8 +2,10 @@ package com;
 
 
 public class StockOrders {
+    public int quantity;
     public void setQuantity(int quantity) {
 //begin of modifiable zone(JavaCode)......C/29d3637f-6a5e-49f1-8b0a-122b11310b19
+        this.quantity = quantity;
 
 //end of modifiable zone(JavaCode)........E/29d3637f-6a5e-49f1-8b0a-122b11310b19
     }
