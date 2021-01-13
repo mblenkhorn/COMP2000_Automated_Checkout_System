bbec9c03325386c98fbcae43f6ae71c733ddcb9b
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-01-13 12:32:55 +0000

    Updated AdminUser.java
    Added functionality for the adminAccessStock method.

--- src/com/AdminUser.java
+++ src/com/AdminUser.java
@@ -4,13 +4,21 @@ package com;
 import javax.swing.*;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AdminUser implements IControllerAdmin, IObserverAdmin {
     public StockOrders stockOrders;
 
+    public ArrayList<Stock> stocks = new ArrayList<Stock>();
+
     public String userName;
 
     public String password;
+
+    String filePath = "Resources\\data.txt";
+    String separator = "\\|";
 
     public void adminLogin(JTextField usernameField, JPasswordField passwordField) {
 //begin of modifiable zone(JavaCode)......C/04c97770-dd62-4da7-8b88-16fea723b0d7
@@ -33,8 +41,43 @@ public class AdminUser implements IControllerAdmin, IObserverAdmin {
 //end of modifiable zone(JavaCode)........E/04c97770-dd62-4da7-8b88-16fea723b0d7
     }
 
-    public void adminAccessStock() {
+    public void adminAccessStock(JTextArea textDisplay) {
 //begin of modifiable zone(JavaCode)......C/cdfb3af3-d9b5-401d-803b-c1f6a61663db
+        try{
+            File file = new File(filePath);
+            Scanner scanner = new Scanner(file);
+
+            while(scanner.hasNextLine())
+            {
+                String tableRow = scanner.nextLine();
+                String[] userData = tableRow.split(separator);
+
+                Stock stock = new Stock();
+
+                int barCodeToInt = Integer.parseInt(userData[0]);
+                stock.setBarCode(barCodeToInt);
+                stock.setName(userData[1]);
+                float priceToFloat = Float.parseFloat(userData[2]);
+                stock.setPrice(priceToFloat);
+
+                stocks.add(stock);
+            }
+
+            scanner.close();
+
+            for(int i = 0; i < stocks.size(); i++)
+            {
+                textDisplay.append(stocks.get(i).getBarCode() + " " + stocks.get(i).getName() + " "
+                        + "£ " +  stocks.get(i).getPrice() + "\n");
+            }
+
+            System.out.print("File Read Successful");
+
+
+        }catch(FileNotFoundException exception){
+            exception.printStackTrace();
+        }
+
 
 //end of modifiable zone(JavaCode)........E/cdfb3af3-d9b5-401d-803b-c1f6a61663db
     }
@@ -110,5 +153,7 @@ public class AdminUser implements IControllerAdmin, IObserverAdmin {
 
 //end of modifiable zone(JavaCode)........E/1498d2f0-bee4-4c02-894b-0948ae2b6ffa
     }
+
+    public void saveStock(){}
 
 }
