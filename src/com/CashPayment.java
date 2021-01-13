436f920db432d5b297c4ed9ae8734b848b71d5a7
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-01-13 18:50:06 +0000

    Updated CashPayment.java
    Edited script so that the CalculateCost method shows the total cost on the screen,

--- src/com/CashPayment.java
+++ src/com/CashPayment.java
@@ -2,22 +2,25 @@ package com;
 
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class CashPayment extends PaymentMethod {
     public float cashChange;
 
-    public void calculateCost(JLabel totalCost) {
+    public void calculateCost(JLabel totalCost, ArrayList<Stock> scannedItems) {
 //begin of modifiable zone(JavaCode)......C/b832e3b0-c1d2-431f-8611-661e891aa9f9
         float cost = 0;
+        float priceCost = 0;
         Customers customer = new Customers();
 
 
-        for(int i = 0; i < 10; i++){
-            cost = cost + 10;
+        for(int i = 0; i < scannedItems.size(); i++){
+            priceCost = 2.50f * 2;
+            cost = priceCost;
         }
 
         String formattedText = String.format("%.02f", cost);
-        totalCost.setText("TOTAL COST: " + formattedText);
+        totalCost.setText(formattedText);
 
 
 //end of modifiable zone(JavaCode)........E/b832e3b0-c1d2-431f-8611-661e891aa9f9
@@ -27,10 +30,7 @@ public class CashPayment extends PaymentMethod {
 //begin of modifiable zone(JavaCode)......C/33a6ad51-8fec-47ee-9f01-6938b52e94cd
         cashChange = 0;
         cashChange = cashAmount - totalPayment;
-
-
         String formattedString = String.format("%.02f", cashChange);
-
         totalCost.setText("Here's your change: " + formattedString);
 
 //end of modifiable zone(JavaCode)........E/33a6ad51-8fec-47ee-9f01-6938b52e94cd
