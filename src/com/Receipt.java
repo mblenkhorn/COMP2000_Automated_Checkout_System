ba9fe2fd5435b47d50ce2f8a5beae7908c0e98f8
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-01-13 21:14:56 +0000

    Updated Receipt.java
    Updated the Receipt to display the name, date, scannedItems, payment method and total price. The Receipt runs on a Thread.

--- src/com/Receipt.java
+++ src/com/Receipt.java
@@ -2,7 +2,10 @@ package com;
 
 
 import javax.swing.*;
+import java.awt.*;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Receipt extends JFrame implements IView {
     public Stock[] stockItems;
@@ -10,16 +13,64 @@ public class Receipt extends JFrame implements IView {
     public String paymentMethod;
 
 
-    public void display(JTextArea scannedStockOutput, ArrayList<Stock> scannedItems) {
-//begin of modifiable zone(JavaCode)......C/8cde6f6e-e2ad-43c1-bb1a-65e63e80abae
-        Receipt receipt = new Receipt();
+    public void printOutReceipt(String payment, JLabel totalCostLabel)
+    {
+        try{
+            Thread.sleep(12000);
+        }catch(InterruptedException exception){
+            exception.printStackTrace();
+        }
 
-//end of modifiable zone(JavaCode)........E/8cde6f6e-e2ad-43c1-bb1a-65e63e80abae
+        new Runnable(){
+            @Override
+            public void run(){
+                System.out.println("My Thread: " + Thread.currentThread().getName());
+                try{
+                    Receipt receipt = new Receipt(payment, totalCostLabel);
+                }catch(Exception error){
+                    error.printStackTrace();
+                }
+            }
+        }.run();
+
     }
 
-    public Receipt() {
+    public Receipt(String payment, JLabel totalCostLabel) {
 //begin of modifiable zone(JavaSuper).....C/d28f0793-daa3-48a3-be60-a7126e021ce6
+        paymentMethod = payment;
+        Customers customers = new Customers();
+        String name = "Obscurus Supermarkets Ltd - We sell invisible items for a very reasonable price!";
         JPanel receiptPanel = new JPanel();
+        JLabel nameLabel = new JLabel();
+        nameLabel.setText(name);
+        JLabel dateLabel = new JLabel();
+        dateLabel.setText(new SimpleDateFormat("dd-MMM-YYYY").format(new Date()));
+        JTextArea itemDisplay = new JTextArea();
+        itemDisplay.setForeground(Color.GRAY);
+        itemDisplay.setEditable(false);
+
+        for(int i = 0; i < 10; i++){
+            itemDisplay.append("Apples " + " " + "2.50" + "\n");
+        }
+
+        JLabel paymentMethodLabel = new JLabel();
+        paymentMethodLabel.setText(paymentMethod);
+        JLabel totalCost = new JLabel();
+        totalCost.setText("£ " + totalCostLabel.getText());
+
+        receiptPanel.add(nameLabel);
+        receiptPanel.add(dateLabel);
+        receiptPanel.add(itemDisplay);
+        receiptPanel.add(paymentMethodLabel);
+        receiptPanel.add(totalCost);
+
+        this.setContentPane(receiptPanel);
+        this.setLayout(new GridLayout(10, 15));
+        this.setPreferredSize(new Dimension(800, 600));
+        this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+
 
 
 //end of modifiable zone(JavaSuper).......E/d28f0793-daa3-48a3-be60-a7126e021ce6
@@ -28,6 +79,8 @@ public class Receipt extends JFrame implements IView {
 //end of modifiable zone(JavaCode)........E/d28f0793-daa3-48a3-be60-a7126e021ce6
     }
 
+    //NOT USED IN PROJECT
     public void print(JLabel outputLbl, JLabel outputLabel){System.out.print("HELLO WORLD");}
+    public void display(JTextArea scannedStockOutput, ArrayList<Stock> scannedItems){System.out.print("HELLO WORLD");}
 
 }
