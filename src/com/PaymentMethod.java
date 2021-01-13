5be9b985a2a6e10e0884f6bed758e7437ed77a43
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-01-13 21:15:22 +0000

    Updated PaymentMethod.java
    Updated methods so that the CardPayment and CashPayment can use it.

--- src/com/PaymentMethod.java
+++ src/com/PaymentMethod.java
@@ -4,7 +4,7 @@ package com;
 import javax.swing.*;
 
 public abstract class PaymentMethod {
-    public abstract void startPayment(JTextField inputMoney, JLabel totalCost);
-    public abstract void startTransaction(JTextField inputCard, JLabel outputLbl);
+    public abstract void startPayment(JTextField inputMoney, JLabel totalCost, JLabel totalCostLabel, JLabel changeLabel);
+    public abstract void startTransaction(JTextField inputCard, JLabel outputLbl, JLabel totalCostLabel, JLabel verifier);
 
 }
