cbc18fd8214ae579dd98543f8902c6ea885ce45d
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-01-13 15:31:30 +0000

    Updated StockDatabase
    added try and catch statements to avoid errors and fixed the functions as they are applicable within the interface. saveStock was removed as it was no longer needed.

--- src/com/StockDatabase.java
+++ src/com/StockDatabase.java
@@ -14,64 +14,57 @@ public class StockDatabase implements IModelSubject {
 
     public void removeStock() {
 //begin of modifiable zone(JavaCode)......C/eead22e9-f6a3-4475-b4a8-a52eebdaafc7
+        try{
+            Stock stockToRemove = new Stock();
+            stockItems.remove(stockToRemove);
+        }
+        catch(Exception error){
+            error.printStackTrace();
+        }
+
 
 //end of modifiable zone(JavaCode)........E/eead22e9-f6a3-4475-b4a8-a52eebdaafc7
     }
 
-    public void addStock(Stock newStock, JTextField barCodeInput, JTextField nameInput, JTextField priceInput) {
+    public void addStock(Stock newStock, JTextField barCodeInput, JTextField nameInput, JTextField priceInput, JTextArea dataDisplay) {
 //begin of modifiable zone(JavaCode)......C/a8d17de0-d8e6-4bdc-85b5-fff935603d17
         newStock.setBarCode(Integer.parseInt(barCodeInput.getText()));
         newStock.setName(nameInput.getText());
         newStock.setPrice(Float.parseFloat(priceInput.getText()));
         stockItems.add(newStock);
+        updateStockDatabase(dataDisplay);
 
 //end of modifiable zone(JavaCode)........E/a8d17de0-d8e6-4bdc-85b5-fff935603d17
     }
 
-    public void editStock() {
+    public void editStock(JTextField stockIndexInput, JTextField barCodeInput, JTextField nameInput, JTextField priceInput) {
 //begin of modifiable zone(JavaCode)......C/7dd1a4c1-90e8-4193-b144-a8c82f8fbbc8
+        try{
+            Stock stock = new Stock();
+            stock.setBarCode(Integer.parseInt(barCodeInput.getText()));
+            stock.setName(nameInput.getText());
+            stock.setPrice(Float.parseFloat(priceInput.getText()));
+            stockItems.add(stock);
+        }
+        catch(Exception error){
+            error.printStackTrace();
+        }
+
+
 
 //end of modifiable zone(JavaCode)........E/7dd1a4c1-90e8-4193-b144-a8c82f8fbbc8
     }
 
-    public void updateStockDatabase() {
+    public void updateStockDatabase(JTextArea textDisplay) {
 //begin of modifiable zone(JavaCode)......C/0cdd81ce-d018-487c-a2a1-e42e31efd7da
+        for(int i = 0; i < stockItems.size(); i++)
+        {
+            textDisplay.append(stockItems.get(i).getBarCode() + " " + stockItems.get(i).getName() + " "
+                    + "£ " +  stockItems.get(i).getPrice() + "\n");
+        }
 
 //end of modifiable zone(JavaCode)........E/0cdd81ce-d018-487c-a2a1-e42e31efd7da
     }
 
-    public void saveStock(){
-        try
-        {
-            FileWriter writer = new FileWriter(filePath);
-
-            for(int index = 0; index < stockItems.size(); index++)
-            {
-                String dataRow = "";
-
-                if(index > 0 )
-                {
-                    dataRow += "\n";
-                }
-
-                String barCodeToString = Integer.toString(stockItems.get(index).getBarCode());
-                dataRow += barCodeToString;
-
-                dataRow += "|" + stockItems.get(index).getName();
-
-                String priceToString = Float.toString(stockItems.get(index).getPrice());
-                dataRow += "|" + priceToString;
-
-                writer.write(dataRow);
-            }
-
-            writer.close();
-            System.out.println("User File Successfully Saved");
-        }
-        catch(IOException exception)
-        {
-            exception.printStackTrace();
-        }
-    }
 
 }
