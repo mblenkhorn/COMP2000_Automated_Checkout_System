3b6caf07768412093bf4a365a17fec25aa048fcf
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-01-13 18:03:38 +0000

    Updated bank.java
    Added a String variable called bankMessage to make the setMessage method work.

--- src/com/Bank.java
+++ src/com/Bank.java
@@ -2,9 +2,10 @@ package com;
 
 
 public class Bank {
+    public String bankMessage;
     public void setMessage(String message) {
 //begin of modifiable zone(JavaCode)......C/9695efcf-32ee-4935-84be-ae8b6c5cea87
-
+        this.bankMessage = message;
 //end of modifiable zone(JavaCode)........E/9695efcf-32ee-4935-84be-ae8b6c5cea87
     }
 
