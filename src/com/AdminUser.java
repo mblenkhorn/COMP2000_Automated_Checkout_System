dd05b6f66ece88c86ab6ddaec3106df53593cd38
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-01-14 12:23:05 +0000

    Updated AdminUser.java
    Added in comments to explain code

--- src/com/AdminUser.java
+++ src/com/AdminUser.java
@@ -9,50 +9,48 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AdminUser implements IControllerAdmin, IObserverAdmin {
-    public StockOrders stockOrders;
+    public StockOrders stockOrders; //this is the orders
 
-    public ArrayList<Stock> stocks = new ArrayList<Stock>();
+    public ArrayList<Stock> stocks = new ArrayList<Stock>(); //this is the stock array that will store the stock
 
-    public String userName;
+    public String userName; //this is the username of the user
 
-    public String password;
+    public String password; //this is the password of the user
 
-    String filePath = "Resources\\data.txt";
-    String separator = "\\|";
+    String filePath = "Resources\\data.txt"; //the filepath needed to read the data.txt file
+    String separator = "\\|"; //separates the data (removes the bar from the data.txt file)
 
     public void adminLogin(JTextField usernameField, JPasswordField passwordField) {
 //begin of modifiable zone(JavaCode)......C/04c97770-dd62-4da7-8b88-16fea723b0d7
-        StockDatabase databaseUser = new StockDatabase();
-        AdminUser user = new AdminUser();
+        StockDatabase databaseUser = new StockDatabase(); //creates a new StockDatabase object called databaseUser
+        AdminUser user = new AdminUser(); //creates a new AdminUser object called user
 
-        for(int index = 0; index < databaseUser.adminUsers.size(); index++)
+        for(int index = 0; index < databaseUser.adminUsers.size(); index++) //before it reaches the size of the adminUsers ArrayList
         {
+            //if the username and password inputs matches the adminUser username and password
             if(usernameField.getText().equals(databaseUser.adminUsers.get(index).userName)
                     && passwordField.getText().equals(databaseUser.adminUsers.get(index).password))
             {
-                System.out.print("Hello World");
+                System.out.print("Hello World"); //tests the correct login
             }
         }
-
-
-
-
-
 //end of modifiable zone(JavaCode)........E/04c97770-dd62-4da7-8b88-16fea723b0d7
     }
 
     public void adminAccessStock(JTextArea textDisplay) {
 //begin of modifiable zone(JavaCode)......C/cdfb3af3-d9b5-401d-803b-c1f6a61663db
         try{
-            File file = new File(filePath);
-            Scanner scanner = new Scanner(file);
+            File file = new File(filePath); //creates a new file object called file and uses the filepath for reading
+            Scanner scanner = new Scanner(file); //creates a new scanner object called scanner and uses the file for scanning
 
-            while(scanner.hasNextLine())
+            while(scanner.hasNextLine()) //while scanner has lines to read
             {
-                String tableRow = scanner.nextLine();
-                String[] userData = tableRow.split(separator);
+                String tableRow = scanner.nextLine(); //the tableRow is the first line read by scanner
+                String[] userData = tableRow.split(separator); //splits the tableRow into different parts
 
-                Stock stock = new Stock();
+                Stock stock = new Stock(); //creates a new stock object called Stock
+
+                //converts the data to a new data type if necessary, sets the data from userData index, and adds the stock to the arrayList
 
                 int barCodeToInt = Integer.parseInt(userData[0]);
                 stock.setBarCode(barCodeToInt);
@@ -65,13 +63,13 @@ public class AdminUser implements IControllerAdmin, IObserverAdmin {
 
             scanner.close();
 
-            for(int i = 0; i < stocks.size(); i++)
+            for(int i = 0; i < stocks.size(); i++) //before it reaches the size of the stock
             {
                 textDisplay.append(stocks.get(i).getBarCode() + " " + stocks.get(i).getName() + " "
                         + "£ " +  stocks.get(i).getPrice() + "\n");
             }
 
-            System.out.print("File Read Successful");
+            System.out.print("File Read Successful"); //test successful login
 
 
         }catch(FileNotFoundException exception){
@@ -84,13 +82,13 @@ public class AdminUser implements IControllerAdmin, IObserverAdmin {
 
     public void replenishStockWarning(JLabel notifier) {
 //begin of modifiable zone(JavaCode)......C/d009cb0c-2980-482c-9d04-4959a9dd8944
-        int count = 0;
+        int count = 0; //creates a int variable called count and gives it the value of 0
 
-        StockDatabase database = new StockDatabase();
+        StockDatabase database = new StockDatabase(); //creates a new StockDatabase object called database
 
-        for(int index = 0; index < database.stockItems.size(); index++)
+        for(int index = 0; index < database.stockItems.size(); index++) //before it reaches the stockitems's size
         {
-            count++;
+            count++; //increment by 1
         }
 
         if(count < 3)
@@ -104,14 +102,15 @@ public class AdminUser implements IControllerAdmin, IObserverAdmin {
     public void orderStock(Stock stockOrder, JLabel outputLbl, JTextArea textDisplay) {
 //begin of modifiable zone(JavaCode)......C/23ef2dd5-ade8-4143-aaa8-862d22f01370
         outputLbl.setText("Your order has been processed!");
-        stockOrders = new StockOrders();
-        stockOrders.setQuantity(3);
+        stockOrders = new StockOrders(); //creates a new StockOrders object
+        stockOrders.setQuantity(3); //runs this function using 3 as an argument
         StockDatabase database = new StockDatabase();
 
         for(int i = 0; i < stockOrders.quantity; i++)
         {
-            stockOrder.setBarCode(1001);
-            stockOrder.setName("Apples");
+            //sets the data and adds it the arrayList
+            stockOrder.setBarCode(1004);
+            stockOrder.setName("Pineapples");
             stockOrder.setPrice(1.24f);
             stocks.add(stockOrder);
         }
@@ -119,11 +118,11 @@ public class AdminUser implements IControllerAdmin, IObserverAdmin {
         database.updateStockDatabase(textDisplay, stocks);
 
         int j = 0;
-        int waitForSeconds = 100;
+        int waitForSeconds = 100; //time for waiting
 
         while(j != waitForSeconds)
         {
-            j++;
+            j++; //increment by 1
         }
 
         updateOnDelivery(outputLbl, textDisplay);
@@ -134,7 +133,7 @@ public class AdminUser implements IControllerAdmin, IObserverAdmin {
     public void updateOnDelivery(JLabel outputLbl, JTextArea textDisplay) {
 //begin of modifiable zone(JavaCode)......C/c314ea4f-fe00-4f3f-8f89-ba7205e74287
         outputLbl.setText("Your order has now been delivered" + " Click on the Database tab!");
-        StockDatabase stockDatabase = new StockDatabase();
+        StockDatabase stockDatabase = new StockDatabase(); //creates a new StockDatabase object
         stockDatabase.updateStockDatabase(textDisplay, stocks);
 
 //end of modifiable zone(JavaCode)........E/c314ea4f-fe00-4f3f-8f89-ba7205e74287
@@ -143,12 +142,12 @@ public class AdminUser implements IControllerAdmin, IObserverAdmin {
     public void createAccount(JTextField usernameField, JPasswordField passwordField) {
 //begin of modifiable zone(JavaCode)......C/1498d2f0-bee4-4c02-894b-0948ae2b6ffa
         try{
-            String filePath = "Resources\\accounts.txt";
-            FileWriter writer = new FileWriter(filePath);
+            String filePath = "Resources\\accounts.txt"; //creates a new String called filePath and gives the filepath to read
+            FileWriter writer = new FileWriter(filePath); //creates a new FileWriter object called writer which will write to the file
 
-            StockDatabase databaseUser = new StockDatabase();
+            StockDatabase databaseUser = new StockDatabase(); //creates a new StockDatabase object called databaseUser
 
-            AdminUser user = new AdminUser();
+            AdminUser user = new AdminUser(); //creates a new AdminUser object called user
             user.userName = usernameField.getText();
             user.password = passwordField.getText();
 
@@ -156,7 +155,7 @@ public class AdminUser implements IControllerAdmin, IObserverAdmin {
 
             for(int index = 0; index < databaseUser.adminUsers.size(); index++)
             {
-                String userAccountDataRow = "";
+                String userAccountDataRow = ""; //creates a new String variable object called userAccountDataRow
 
                 if(index > 0)
                 {
@@ -166,7 +165,7 @@ public class AdminUser implements IControllerAdmin, IObserverAdmin {
                 userAccountDataRow += "|" + databaseUser.adminUsers.get(index).userName;
                 userAccountDataRow += "|" + databaseUser.adminUsers.get(index).password;
 
-                writer.write(userAccountDataRow);
+                writer.write(userAccountDataRow); //writes the userData to the file
             }
 
             writer.close();
