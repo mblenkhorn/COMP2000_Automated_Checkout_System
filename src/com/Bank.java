dadd437bb1a01851cee0380d31b6fb0cd94499ac
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-01-14 12:23:55 +0000

    Updated Bank.java
    Added comments to explain code.

--- src/com/Bank.java
+++ src/com/Bank.java
@@ -2,10 +2,10 @@ package com;
 
 
 public class Bank {
-    public String bankMessage;
+    public String bankMessage; //creates a new String variable called bankMessage
     public void setMessage(String message) {
 //begin of modifiable zone(JavaCode)......C/9695efcf-32ee-4935-84be-ae8b6c5cea87
-        this.bankMessage = message;
+        this.bankMessage = message; //sets message to the this.bankMessage
 //end of modifiable zone(JavaCode)........E/9695efcf-32ee-4935-84be-ae8b6c5cea87
     }
 
